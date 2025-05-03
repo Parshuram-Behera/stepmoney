de5a3a66886f92bbb89783f6f5c619bb30b801ba
Parshuramb7 <parshurambehera7735@gmail.com>
2025-05-03 11:23:35 +0530

    push Step Money

---
+++ app/src/main/java/com/OneDevApps/stepmoney/Onboard/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.OneDevApps.stepmoney.Onboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.OneDevApps.stepmoney.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
